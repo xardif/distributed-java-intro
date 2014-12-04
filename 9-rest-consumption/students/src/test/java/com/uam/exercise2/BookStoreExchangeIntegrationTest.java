9c8916bcddc59f7c47f1c13e7fe471a3430149c9
Paweł Kruszczyński <xardif@gmail.com>
2014-12-04 09:45:13 +0100

    eTag tests

--- 9-rest-consumption/students/src/test/java/com/uam/exercise2/BookStoreExchangeIntegrationTest.java
+++ 9-rest-consumption/students/src/test/java/com/uam/exercise2/BookStoreExchangeIntegrationTest.java
@@ -1,16 +1,15 @@
 package com.uam.exercise2;
 
 import com.uam.model.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.uam.Application;
@@ -77,5 +76,39 @@ public class BookStoreExchangeIntegrationTest {
         System.out.println(response.getHeaders());
         System.out.println(response.getBody());
 	}
+
+
+    @Test
+    public void eTagTest() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        HttpEntity entity = new HttpEntity(headers);
+        ResponseEntity response = restTemplate.exchange(Book.URL + "?title=Shining", HttpMethod.GET, entity, String.class);
+        String eTag = response.getHeaders().getETag();
+        System.out.println(eTag);
+
+        headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        headers.set("If-None-Match", eTag);
+        entity = new HttpEntity(headers);
+
+        response = restTemplate.exchange(Book.URL + "?title=Shining", HttpMethod.GET, entity, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        response = restTemplate.exchange(Book.URL + "?title=MyBook", HttpMethod.GET, entity, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        response = restTemplate.exchange(Book.URL + "?title=Shining22", HttpMethod.GET, entity, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+    }
 }
 
